1e6f5f561e44536ef82cdd67caada2430c623913
TechnologieSofsoftware <34411142+TechnologieSofsoftware@users.noreply.github.com>
2017-12-10 02:03:40 -0600

    Create salidadepantalla.java

---
+++ Java/salidadepantalla.java
@@ -0,0 +1,47 @@
+package technologiesofsoftware.blogspot.mx;
+
+/**
+ *
+ * @author AlejandroGS13
+ *http://technologiesofsoftware.blogspot.mx
+ */
+public class TechnologiesofsoftwareBlogspotMx {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        Simple sout mas comillas dobles 
+        */
+        System.out.println("Hola Mundo! :)\"");
+        /*
+        Sout con salto de linea en la misma sentencia
+        */
+        System.out.println("Hola Mundo! :) \" \n http://technologiesofsoftware.blogspot.mx/");
+        /*
+        salidas a pantalla en diferentes sentencias pero la salida en la misma linea
+        */
+        System.out.print("Hola");   
+        System.out.print(" Mundo");
+        System.out.print(" ¿Como ");
+        System.out.print("Esta?\n");
+        /*
+        Salida a pantalla de una constante
+        */
+        System.out.println(2017);
+        /*
+        Concatenacion de cadenas y constantes en un sout   
+        */
+        System.out.println("Hoy es "+10+" de diciembre del "+2017);
+        /*
+        Ejemplo de todo lo anterior 
+        */
+        System.out.print("Hola ");
+        System.out.print("Mundo ");
+        System.out.print(" :\")");
+        System.out.println("Mi nombre es:\nDavid Alejandro \nTengo "+23
+            + "años \n"+"Y amo programar");
+    }
+    
+}
